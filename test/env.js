d62471d7ef99f10414208543b52eef940b8237ff
James Halliday <mail@substack.net>
2012-07-12 03:08:58 -0700

    passing test for array env

--- test/env.js
+++ test/env.js
@@ -25,3 +25,27 @@ test('env', function (t) {
     });
     t.end();
 });
+
+test('array env', function (t) {
+    function config (dir) {
+        return {
+            dir : path.join(__dirname, dir),
+            env : [ 'boop', 'beep' ]
+        };
+    }
+    
+    t.same(confuse(config()), {
+        W: 777, q: { w: 7 }, n: 3
+    });
+    t.same(confuse(config('foo')), {
+        W: 777, q: { w: 7, z: 616 }, n: 2, Z: 61616
+    });
+    t.same(confuse(config('foo/bar')), {
+        W: 777, q: { w: 7, z: 616, y: 5.5 }, n: 1, Z: 61616, Y: 555
+    });
+    t.same(confuse(config('foo/bar/baz')), {
+        W: 777, q: { w: 7, z: 616, y: 5.5, x: 4 }, n: 0,
+        Z: 61616, Y: 555, X: 444
+    });
+    t.end();
+});
