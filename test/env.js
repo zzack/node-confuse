8f772f6c742d75cbfbdc816aa2c16ae49ae56826
James Halliday <mail@substack.net>
2012-07-12 03:05:21 -0700

    failing env test

---
+++ test/env.js
@@ -0,0 +1,27 @@
+var test = require('tap').test;
+var confuse = require('../');
+var path = require('path');
+
+test('env', function (t) {
+    function config (dir) {
+        return {
+            dir : path.join(__dirname, dir),
+            env : 'beep'
+        };
+    }
+    
+    t.same(confuse(config()), {
+        W: 777, q: { w: 7 }, n: 3
+    });
+    t.same(confuse(config('foo')), {
+        W: 777, q: { w: 7, z: 606 }, n: 2, Z: 60606
+    });
+    t.same(confuse(config('foo/bar')), {
+        W: 777, q: { w: 7, z: 6, y: 5 }, n: 1, Z: 666, Y: 555
+    });
+    t.same(confuse(config('foo/bar/baz')), {
+        W: 777, q: { w: 7, z: 606, y: 5, x: 4 }, n: 0,
+        Z: 60606, Y: 555, X: 444
+    });
+    t.end();
+});
