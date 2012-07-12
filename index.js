9a7c76422aff4e4ece56871230e8eb8325ad414c
Zack Story <zack@zack.local>
2012-07-12 09:46:16 -0700

    consistent whitespace

--- index.js
+++ index.js
@@ -6,22 +6,22 @@ var figc = require('figc');
 var optimist = require('optimist');
 
 var exists = fs.existsSync || path.existsSync;
-
+    
 var merge = require('./node_modules/figc/node_modules/deepmerge')
-
+    
 module.exports = function (opts) {
     if (typeof opts === 'string') {
         opts = { env : opts };
     }
     if (!opts) opts = {};
     var prefix = opts.prefix || 'config';
-
+    
     var files = opts.files ||
         [].concat(opts.env || []).map(function (e) {
             return prefix + '.' + e + '.json'
         }).concat(prefix + '.json')
     ;
-
+    
     var dirs = parents(opts.dir || path.dirname(require.main.filename));
     var configFiles = []
     dirs.map(function (dir) {
@@ -34,9 +34,9 @@ module.exports = function (opts) {
         .filter(Boolean)
     ;
     var argv = opts.argv || optimist.argv;
-
+    
     if (configFiles.length === 0) return argv;
-
+    
     return configFiles.reduce(function (config, file) {
         //if the file is .js assume it will export a configuration object to be merged
         if(file.split('.').pop() == 'js'){
